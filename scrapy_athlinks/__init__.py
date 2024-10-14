dca1a0044a2efa3c1902e872ff2024fac60da815
Jose Vicente Nunez <josevnz@kodegeek.com>
2024-10-13 20:03:47 -0400

    Types hint

--- scrapy_athlinks/__init__.py
+++ scrapy_athlinks/__init__.py
@@ -2,14 +2,13 @@
 Use __all__ to let type checkers know what is part of the public API.
 The public API is determined based on the documentation.
 """
+import importlib.metadata
 from scrapy_athlinks.spiders.race import RaceSpider
 from scrapy_athlinks.items import RaceItem, AthleteItem, AthleteSplitItem
 
-import importlib.metadata
-
 __version__ = importlib.metadata.version("scrapy_athlinks")
 
-__all__ = [ 
+__all__ = [
   'AthleteItem',
   'AthleteSplitItem',
   'RaceItem',
