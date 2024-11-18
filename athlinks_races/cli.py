dfbdd77176bd84a575b0c0fabc4e078464cc49c7
Jose Vicente Nunez <josevnz@kodegeek.com>
2024-11-17 21:06:00 -0500

    First cut of TUI. Keeping vanilla flavor

--- athlinks_races/cli.py
+++ athlinks_races/cli.py
@@ -1,18 +1,21 @@
 """
 Command line interface parser
 """
+import logging
 from argparse import ArgumentParser
 from pathlib import Path
 
 from scrapy.crawler import CrawlerProcess
+from textual.logging import TextualHandler
+
 from athlinks_races import RaceSpider, AthleteItem, RaceItem
+from athlinks_races.app import RaceCrawler
 
 
 def main():
     """
     CLI entry point
     Returns:
-
     """
 
     parser = ArgumentParser(description=__doc__)
@@ -48,35 +51,53 @@ def main():
         default=default_url,
         help=f"Race to crawl (Default: Crawl results for the 2022 Leadville Trail 100 Run, {default_url})",
         nargs="?"
-
+    )
+    parser.add_argument(
+        '--tui',
+        action="store_true",
+        help="If passed, enable rich TUI with a post run report."
     )
     options = parser.parse_args()
 
     # Make settings for two separate output files: one for athlete data,
     # one for race metadata.
     settings = {
-      'FEEDS': {
-        # Athlete data. Inside this file will be a list of dicts containing
-        # data about each athlete's race and splits.
-        options.athletes_rpt.as_posix(): {
-          'format': options.format,
-          'overwrite': True,
-          'item_classes': [AthleteItem],
-        },
-        # Race metadata. Inside this file will be a list with a single dict
-        # containing info about the race itself.
-        options.metadata_rpt.as_posix(): {
-          'format': 'json',
-          'overwrite': True,
-          'item_classes': [RaceItem],
-        },
-      }
+        'FEEDS': {
+            # Athlete data. Inside this file will be a list of dicts containing
+            # data about each athlete's race and splits.
+            options.athletes_rpt.as_posix(): {
+                'format': options.format,
+                'overwrite': True,
+                'item_classes': [AthleteItem],
+            },
+            # Race metadata. Inside this file will be a list with a single dict
+            # containing info about the race itself.
+            options.metadata_rpt.as_posix(): {
+                'format': 'json',
+                'overwrite': True,
+                'item_classes': [RaceItem],
+            },
+        }
     }
-    process = CrawlerProcess(settings=settings)
-
-    process.crawl(RaceSpider, options.race_url)
-    process.start()
+    crawler_process = CrawlerProcess(settings=settings)
+    crawler_process.crawl(RaceSpider, options.race_url)
+    if not options.tui:
+        crawler_process.start()
+    else:
+        logging.basicConfig(
+            level="INFO",
+            handlers=[
+                TextualHandler()
+            ],
+        )
+        app = RaceCrawler(athletes_file=options.athletes_rpt)
+        app.title = "athlinks-races"
+        app.crawler_process = crawler_process
+        app.run(inline_no_clear=True)
 
 
 if __name__ == "__main__":
+    """
+    Entry point
+    """
     main()
